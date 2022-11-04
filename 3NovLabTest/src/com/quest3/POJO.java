9f08cb54fe4735c5738496edc8ac5505feddd7c2
ankitpatelg <107167053+ankitpatelg@users.noreply.github.com>
2022-11-03 21:47:21 -0700

    Add files via upload

---
+++ 3NovLabTest/src/com/quest3/POJO.java
@@ -0,0 +1,60 @@
+/* Question 1: Piggie Bank
+ @Author: Pranjal Rajeev
+ @Date: 3 Nov 2022
+ */
+
+//package
+package com.quest3;
+
+//creating class
+public class POJO {
+	
+	//declaring instance variables
+	private int addamt;
+	private int amount;
+	
+	
+	//default constructor
+	public POJO() 
+	{
+		super();
+		// TODO Auto-generated constructor stub
+		System.out.println("DEFAULT CONSTRUCTOR");
+		this.amount=50;
+	}
+
+
+
+	//Parameterised constructor
+	public POJO(int addamt, int amount) 
+	{
+		System.out.println("PARAMERISED CONSTRUCTOR");
+		this.addamt = addamt;
+		this.amount = amount;
+	}
+
+
+
+	public int getAddamt() {
+		return addamt;
+	}
+
+
+
+	public int setAddamt(int addamt) {
+		return this.addamt = addamt;
+	}
+
+
+
+	public int getAmount() {
+		return amount;
+	}
+
+
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	
+}
---
+++ 3NovLabTest/src/com/quest3/PiggieBank.java
@@ -0,0 +1,49 @@
+/* Question 1: Piggie Bank
+ @Author: Pranjal Rajeev
+ @Date: 3 Nov 2022
+ */
+
+//Package
+package com.quest3;
+
+//importing scanner
+import java.util.Scanner;
+
+//creating class
+public class PiggieBank {
+
+
+	//main method
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.println("WELCOME TO YOUR PIGGIE BANK");
+		
+		POJO c1 = new POJO();
+		c1.setAmount(50);
+		
+		int addamt;
+		
+		int choice;
+		System.out.println("Do you want to add more: ");
+		choice = sc.nextInt();
+		
+		//switch case
+		switch(choice)
+		{
+		case 1:System.out.println("Enter the amount: ");
+			addamt = sc.nextInt();
+			c1.setAddamt(addamt);
+			System.out.println("AMOUNT ADDED SUCCESSFULLY");
+			System.out.print("Your Amount is: "+c1.getAddamt());
+			break;
+		case 2:System.out.print("Your Amount is: "+c1.getAmount());
+			break;
+		default:
+			System.out.println("Invalid Input....");
+		}//end of switch
+
+	}//end of main
+
+}//end of class
